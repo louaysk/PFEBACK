fe82ad98f145d56c2a9a58ad8f8ae7421e05afa8
Louay sakhri <Louaysk723@gmail.com>
2020-05-01 02:31:43 +0100

    Ajoutez des fichiers projet.

---
+++ WebAPI/Models/Dto/UserAddDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models.Dto
+{
+    public class UserAddDto
+    {
+
+        public string Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public UserAddDto(string id ,string f, string e, string p, string u)
+        {
+            this.Id = id;
+            this.FullName = f;
+            this.Email = e;
+            this.PhoneNumber = p;
+            this.UserName = u;
+        }
+
+    }
+}
